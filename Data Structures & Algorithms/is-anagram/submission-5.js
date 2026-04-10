7697566bff80241eb75dd6890d5af3b89b6594ae
Dat Nguyen <ngocdatnguyen2404@gmail.com>
2026-04-10 09:51:22 +0700

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length) return false;
+
+        let count = {}
+        for(let i of s) {
+            count[i] = (count[i] || 0) + 1;
+        }
+
+        for(let char of t) {
+            if(!count[char]) return false;
+            count[char]--;
+        }
+        return true
+    }
+}
